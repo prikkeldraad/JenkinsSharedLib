de75c66e3a8cfd63e34fbc8f3f52f22b3c8c45fa
prikkeldraad <prikkeldraad@gmail.com>
2020-07-09 09:57:33 +0200

    Reformatting layout to make testing possible

---
+++ src/test/nl/prikkeldraad/git/VersionParser.groovy
@@ -0,0 +1,29 @@
+import nl.prikkeldraad.git.VersionParser
+
+class VersionParserTestCase extends GroovyTestCase {
+    private vp
+    
+    void setUp() {
+        this.vp = new VersionParser(
+            "master, 
+            "1.2.3", 
+            1234)
+                
+        this.vp.parse()
+    }
+    
+    void testFullFormat() {
+        def number = this.vp.format("%M.%m.%p-%b")
+        assertEquals(number, "1.2.3-1234")
+    }    
+
+    void testNumberFormat() {
+        def number = this.vp.format("%M.%m.%p")
+        assertEquals(number, "1.2.3")
+    }    
+    
+    void testMajorFormat() {
+       def number = this.vp.format("%M")
+       assertEquals(number, "1")
+    }
+}
