a6c5c9798b9dd886979416adaac50eeba1a4949c
Prikkeldraad <prikkeldraad@gmail.com>
2021-09-09 10:13:03 +0200

    Update VersionParserTest.groovy
    added test for branchename parsing

--- src/test/groovy/git/VersionParserTest.groovy
+++ src/test/groovy/git/VersionParserTest.groovy
@@ -10,6 +10,12 @@ class VersionParserTestCase extends GroovyTestCase {
             1234)
                 
         this.vp.parse()
+        
+        this.vp2 = new VersionParser(
+            "IT-2022 Grate the cheese",
+            "2.3.21",
+            80
+        )
     }
 
     void testFullFormat() {
@@ -25,5 +31,10 @@ class VersionParserTestCase extends GroovyTestCase {
     void testMajorFormat() {
        def number = this.vp.format("%M")
        assertEquals(number, "1")
+    }
+    
+    void testBranchName() {
+        def number = this.vp2.format("%M.%m.%p-%b)
+        assertEquals(number, "2.3.21-80"_
     }
 }
